18f97b61ab8083998811c915a8bc352dfd21bb22
valiboy2002 <100294003+valiboy2002@users.noreply.github.com>
2022-07-13 14:17:23 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,115 +1,58 @@
-/* eslint-disable jsx-a11y/alt-text */
+import {useEffect, useState} from 'react'; 
 import './App.css';
-import husky from './husky.svg'           
-import dalmatian from './dalmatian.svg'
-import {useEffect, useState} from 'react'
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+//dc309080
+const API_URL ='http://www.omdbapi.com?apikey=dc309080'
 
-const Rasa=(props)=>{
-return (
-  <>
- <h1> Nume: {props.name} </h1>
-  <img src={props.img} className="App-logo"/>
-  </> 
-)
+function App  ()  {
+const[movies, setMovies]=useState([]);
+const [searchTerm, setSearchTerm]=useState('');
+const searchMovies= async (title)=> {
+const response = await fetch(`${API_URL}&s=${title}`); 
+const data= await response.json();
+
+setMovies(data.Search);
 }
+ useEffect(()=>{
+    searchMovies('Spiderman');
+    },[]);
+    return (
+    <div className="app">
+    <h1>MovieTonight?</h1>
 
+<div className="search">
+<input 
+placeholder="Search movies..."
+value={searchTerm}
+onChange={(e)=>setSearchTerm(e.target.value)}
+/>
+<img 
+src={SearchIcon}
+alt="search"
+onClick={()=>searchMovies(searchTerm)}
+/>
+</div>
 
-function App(){
- const [huskyCounter,sethuskyCounter]=useState(0);
- const [dalmatCounter,setdalmatCounter]=useState(0);
-useEffect(()=>{
-alert("mama are mere")  },
-  [dalmatCounter]
-);
- const tooLessDogs = () => {
-  alert("You should buy 0 or more dogs ;)");
-}
- const tooMuchDogs = () => {
-    alert("You can't buy too much dogs");
- }
-
-  return (
-  
-    <div className="App" >
-      DOGS 
-
-<h1 className='App-header'>  
-
-
-  <Rasa name={"Husky"}
-  className="App-logo"
- 
-  img={husky}
-  />
-
-<button
-className='App-button'
- onClick={()=>sethuskyCounter((prevCount)=>{
-  if (prevCount>0) return prevCount-1; 
-  else {tooLessDogs();
-    return 0;}}
-  )}>
-  -</button>
-
-I wanna {huskyCounter} huskies!
-<button 
-className='App-button'
-onClick={()=>sethuskyCounter((prevCount)=>{
-  if(prevCount<15)return prevCount+1;
-  else { tooMuchDogs();
-  return 15;}
-}
-  )}>
-  +</button>
-  
-  <a
-    //aici pot pune link-uri
-    className="App-link"
-    href="https://www.youtube.com/watch?v=5F6ULZBf0g8"
-    target="_blank"
-    rel="noopener noreferrer"
-    >
-      Click here to see more huskies...
-    </a>
-   
-  <Rasa   name={"Dalmatians"}
-  className="App-logo"
- 
-  img={dalmatian}
-  />
-
-<button
-className='App-button'
- onClick={()=>setdalmatCounter((prevCount)=>{
-  if (prevCount>0) return prevCount-1; 
-  else {tooLessDogs();
-    return 0;}}
-  )}>
-  -</button>
-
-I wanna {dalmatCounter} dalmatians!
-<button 
-className='App-button'
-onClick={()=>setdalmatCounter((prevCount)=>{
-  if(prevCount<15)return prevCount+1;
-  // else { tooMuchDogs();
-  // return 15;}
-}
-  )}>
-  +</button>
+{
+    movies?.length > 0
+    ?(
+        <div className="container">
+            {movies.map((movie)=>(<MovieCard movie={movie}/>))}
+</div>
+    ) :
     
-  </h1>
-
-{/* <h1 className='App-header'>
- {/* <Rasa  
-   className="App-logo"
-  name={"Dalmatian"}
-  img={dalmatian}
-  /> */
+    (
+        <div className="empty">
+            <h2>No movies found</h2>
+        </div>
+        
+    )
 }
-      {/* </h1> */} 
+
+
+
     </div>
-  );
+    );
 }
-
 export default App;
